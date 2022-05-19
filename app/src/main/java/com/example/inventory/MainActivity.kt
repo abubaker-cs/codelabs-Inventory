ebe115a6d49846d102388cd4e1a190e3a58b011d
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-05-20 00:17:28 +0500

    Restructured folders.

--- app/src/main/java/com/example/inventory/MainActivity.kt
+++ app/src/main/java/com/example/inventory/MainActivity.kt
@@ -31,9 +31,12 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         // Retrieve NavController from the NavHostFragment
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
         navController = navHostFragment.navController
+
         // Set up the action bar for use with the NavController
         setupActionBarWithNavController(this, navController)
+
     }
 
     /**
